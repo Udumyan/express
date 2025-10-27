e8075583920ef98759d91ee6947a3968a4602705
Udumyan <ogtaterogtater55@gmail.com>
2025-10-28 01:18:37 +0400

    express_start

---
+++ helpers/createPath.js
@@ -0,0 +1,11 @@
+// const fs = require("fs").promises;
+// const path = require("path");
+
+// const createPath = async (...arg) => {
+//   return await fs.readFile(
+//     path.join(__dirname, "..", ...arg.join().replaceAll(",", "/")),
+//     "utf8"
+//   );
+// };
+
+// module.exports.createPath = createPath;
---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs").promises;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).set({ "content-type": "text/html" });
+  res.sendFile(path.join(__dirname, "pages", "index.html"));
+});
+
+app.get("/api/users", async (req, res) => {
+  const { name, age } = req.query;
+
+  const data = await fs.readFile(
+    path.join(__dirname, "db", "users.json"),
+    "utf8"
+  );
+
+  let users = JSON.parse(data);
+
+  if (name) {
+    users = users.filter((user) =>
+      user.name.toLowerCase().includes(name.toLowerCase())
+    );
+  }
+  if (age) {
+    users = users.filter((user) => user.age === +age);
+  }
+  if (users.length === 0) {
+    return res
+      .status(404)
+      .set({
+        "content-type": "text/html",
+      })
+      .sendFile(path.join(__dirname, "pages", "notFound.html"));
+  }
+  res
+    .status(200)
+    .set({
+      "content-type": "application/json",
+    })
+    .json(users);
+});
+app.get("/api/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const data = await fs.readFile(
+    path.join(__dirname, "db", "users.json"),
+    "utf8"
+  );
+  const users = JSON.parse(data);
+
+  const user = users.find((u) => u.id === +id);
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).set({
+      "content-type": "text/html",
+    });
+    res.sendFile(path.join(__dirname, "pages", "notFound.html"));
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Runing");
+});
